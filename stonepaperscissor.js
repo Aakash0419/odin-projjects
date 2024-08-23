1ac4ef00cfb9fbca0abe03d4e3d536f0a0022f1c
Aakash S <aakashsrrm@gmail.com>
2024-08-23 12:10:13 +0530

    adding loop and adding necessary  changes

--- stonepaperscissor.js
+++ stonepaperscissor.js
@@ -3,22 +3,24 @@ const ifcontains = (userchoices , availablechoices)=>{
         if(availablechoices[i].toLowerCase()===userchoices.toLowerCase()){
             return true;
         }
-        else false;
+        
     }
+    return false;
 }
+/*got some fixing to do*/
 const  spc= () => {
+    console.log("Start game");
+    while(input!=quit){
     let choices = ["rock" , "paper" , "scissors"];
     let random = choices[Math.floor(Math.random()*choices.length)];
+    /*getting random number from 1 to length of choice , then using floor function 
+    on it to store and use as index for choices array*/ 
     let userchoice = prompt("Enter a user choice");
     if(!ifcontains(userchoice,choices)){
         console.log("Invalid choice");
         spc();
     }
     userchoice = userchoice.toLowerCase();
-    
-    
-    
-
     console.log(`Your choice:${userchoice}\t Computer's choice:${random}`);
 
     if (random===userchoice){
@@ -33,7 +35,9 @@ const  spc= () => {
     else {
         console.log("computer wins");
     }
-
-
+    input = window.prompt("Do u want to quit");
+    input = input.toLowerCase();
+    if(input!=quit){alert("invalid choice , continuing game"};
+    }
 }
 spc();
